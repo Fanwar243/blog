4722e5945ae6610c97a20a40c9777f03a23c76f1
Farid Anwar <74921087+Fanwar243@users.noreply.github.com>
2021-05-03 05:48:02 +0800

    Separated script, changed palette, misc

---
+++ blogscript.js
@@ -0,0 +1,41 @@
+function openNav() {
+    var x = window.matchMedia("(max-width: 900px)")
+    if (x.matches) {
+        document.getElementById("nav").style.width = "40%";
+        document.getElementById("nav").style.height = "60%";
+    } else {
+        document.getElementById("nav").style.width = "20%";
+    }
+    document.getElementById("openbtn").style.display = "None";
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0";
+    document.getElementById("openbtn").style.display = "Block";
+}
+
+function expand() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+
+    dots.style.display = "none";
+    moreText.style.display = "inline";
+}
+
+function collapse() {
+    var dots = document.getElementById("dots");
+    var close = document.getElementById("dots2");
+    var moreText = document.getElementById("more");
+    
+    moreText.style.display = "none";
+    dots.style.display = "inline";
+}
+
+const update = () => {
+    const newBook = document.createElement("li");
+    const bookInfo = `${document.getElementById("bookTitle").value} by ${document.getElementById("bookAuthor").value}`;
+    newBook.appendChild(document.createTextNode(bookInfo));
+    document.getElementById("bookList").appendChild(newBook);
+    alert(`Updated with: ${document.getElementById("bookTitle").value} by 
+        ${document.getElementById("bookAuthor").value}`);
+}
