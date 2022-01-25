b3e528fdedd0e948594fa9b167cabe9115c42164
Farid Anwar <faridanwar243@gmail.com>
2022-01-25 14:15:17 +0000

    Update blogscript.js

--- blogscript.js
+++ blogscript.js
@@ -1,11 +1,9 @@
 function openNav() {
-    const x = window.matchMedia("screen and (max-device-width: 900px)")
+    /*const x = window.matchMedia("screen and (max-device-width: 900px)")
     if (x.matches) {
-        document.getElementById("nav").style.width = "50%";
-        document.getElementById("nav").style.height = "60%";
-    } else {
-        document.getElementById("nav").style.width = "20%";
-    }
+        document.getElementById("nav").style.width = "100%";
+    }*/
+    document.getElementById("nav").style.width = "100%";
     document.getElementById("openbtn").style.display = "None";
 }
 
