d6e3b53efb3a8814a85f1d0451f946dac61a003c
Farid Anwar <faridanwar243@gmail.com>
2022-05-29 00:52:19 +0100

    Dark Mode 1.0

--- blogscript.js
+++ blogscript.js
@@ -40,7 +40,6 @@ function expand() {
 function collapse() {
     const dots = document.getElementById("dots");
     const moreText = document.getElementById("more");
-    
     moreText.style.display = "none";
     dots.style.display = "inline";
 }
@@ -54,4 +53,53 @@ const update = () => {
     newBook.appendChild(document.createTextNode(bookInfo));
     document.getElementById("bookList").appendChild(newBook);
     alert(`Updated with: ${document.getElementById("bookTitle").value} by ${document.getElementById("bookAuthor").value}`);
+}
+
+//DARK MODE
+
+let inDarkMode = false;
+
+function darkmode() {
+    if (!inDarkMode) {
+        inDarkMode = true;
+
+        document.getElementById("darkmode").innerHTML = "🌣";
+        document.getElementsByTagName("body")[0].style.backgroundColor = "#121212";
+        document.getElementsByTagName("body")[0].style.color = "white";
+        headings = Array.from(document.getElementsByTagName("h2"));
+        headings.forEach((element) => {
+            element.style.opacity = "70%";
+        });
+        text = Array.from(document.getElementsByTagName("p"));
+        text.forEach((element) => {
+            element.style.opacity = "50%";
+        });
+        lists = Array.from(document.getElementsByTagName("ul"));
+        lists.forEach((element) => {
+            element.style.opacity = "50%";
+        })
+        document.getElementById("links").style.backgroundColor = "#303030"
+        document.getElementById("reading").style.backgroundColor = "#424242"
+        document.getElementsByTagName("footer")[0].style.backgroundColor = "#303030";
+        document.getElementsByTagName("footer")[0].style.color = "white";
+    } else {
+        inDarkMode = false;
+
+        document.getElementById("darkmode").innerHTML = "☾";
+        document.getElementsByTagName("body")[0].style.backgroundColor = "#F4EBD0";
+        document.getElementsByTagName("body")[0].style.color = "#122620";
+        headings.forEach((heading) => {
+            heading.style.opacity = "100%";
+        });
+        text.forEach((paragraph) => {
+            paragraph.style.opacity = "100%";
+        });
+        lists.forEach((list) => {
+            list.style.opacity = "100%";
+        })
+        document.getElementById("links").style.backgroundColor = "#D6AD60";
+        document.getElementById("reading").style.backgroundColor = "tan"
+        document.getElementsByTagName("footer")[0].style.backgroundColor = "#D6AD60";
+        document.getElementsByTagName("footer")[0].style.color = "#122620";
+    }   
 }
